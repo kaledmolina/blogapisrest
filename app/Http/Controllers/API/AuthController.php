0af68331cb82ad86c862064bb8d777c42d182a68
kaled molina <kaled.molina@campusucc.edu.co>
2023-10-17 18:04:50 -0500

    login register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,59 +10,111 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     //funcion para registrar los usuarios
-    public function regiser (Request $request){
-      //validamos los datos
+    public function register(Request $request){
+        //validamos los datos
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed|min:6'
         ]);
-       //crear el usuario
-         $user = new User([
-              'name' => $request->name,
-              'email' => $request->email,
-              'password' => bcrypt($request->password)
-         ]);
 
-        //retornar el usuario y el token
-        return response()->json([
-            'token'=> $user->createToken('api_token')->plainTextToken,
-            'message' => 'Usuario creado correctamente',
-            'user' => $user
-        ], 201);
+        try {
+            //crear el usuario
+            $user = new User([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+
+            //guardar el usuario
+            $user->save();
+
+            //retornar el usuario y el token
+            return response()->json([
+                'token'=> $user->createToken('api_token')->plainTextToken,
+                'message' => 'Usuario creado correctamente',
+                'user' => $user
+            ], 200);
+        } catch (\Exception $e) {
+            //manejar la excepción
+            return response()->json([
+                'message' => 'Hubo un error al crear el usuario: ' . $e->getMessage()
+            ], 500);
+        }
     }
-    public function login (Request $request){
-         //validamos los datos
+
+    public function login(Request $request){
+        //validamos los datos
         $request->validate([
             'email' => 'required|email',
             'password' => 'required|min:6'
         ]);
-       //logn del usuario
-        if(!Auth::attempt($request->only('email', 'password'))){
-            return response()->json([
-                'message' => 'Credenciales incorrectas'
-            ], 403);
-        }
 
-        //retornar el usuario y el token
-        return response()->json([
-            'token'=> auth()->user()->createToken('api_token')->plainTextToken,
-            'message' =>'Usuario logueado correctamente',
-            'user' => auth()->user()
-        ], 200);
+        try {
+            //login del usuario
+            if(!Auth::attempt($request->only('email', 'password'))){
+                return response()->json([
+                    'message' => 'Credenciales incorrectas'
+                ], 401);
+            }
+
+            //retornar el usuario y el token
+            return response()->json([
+                'token'=> auth()->user()->createToken('api_token')->plainTextToken,
+                'message' =>'Usuario logueado correctamente',
+                'user' => auth()->user()
+            ], 200);
+        } catch (\Exception $e) {
+            //manejar la excepción
+            return response()->json([
+                'message' => 'Hubo un error al iniciar sesión: ' . $e->getMessage()
+            ], 500);
+        }
     }
-    public function logout (Request $request){
-        //eliminamos el token
-        auth()->user()->tokens()->delete();
-        //retornamos el mensaje
-        return response()->json([
-            'message' => 'Sesion cerrada correctamente'
-        ], 200);
+
+    public function logout(Request $request){
+        try {
+            //verificar si el usuario está autenticado
+            if(!auth()->check()){
+                return response()->json([
+                    'message' => 'No autenticado'
+                ], 401);
+            }
+
+            //eliminamos el token
+            auth()->user()->tokens()->delete();
+
+            //retornamos el mensaje
+            return response()->json([
+                'message' => 'Sesion cerrada correctamente'
+            ], 200);
+        } catch (\Exception $e) {
+            //manejar la excepción
+            return response()->json([
+                'message' => 'Hubo un error al cerrar la sesión: ' . $e->getMessage()
+            ], 500);
+        }
     }
-    public function user (){
-        //retornamos el usuario
-        return response()->json([
-            'user' => auth()->user()
-        ], 200);
+
+    public function user(){
+        try {
+            //verificar si el usuario está autenticado
+            if(!auth()->check()){
+                return response()->json([
+                    'message' => 'No autenticado'
+                ], 401);
+            }
+
+            //retornamos el usuario
+            return response()->json([
+                'user' => auth()->user()
+            ], 200);
+        } catch (\Exception $e) {
+            //manejar la excepción
+            return response()->json([
+                'message' => 'Hubo un error al obtener el usuario: ' . $e->getMessage()
+            ], 500);
+        }
     }
+
 }
