d07bbd83afe46a8e66b1ce6663b3d51e4553dca1
kaled molina <kaled.molina@campusucc.edu.co>
2023-10-19 17:38:26 -0500

    metodos para los controladores

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,92 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+   //todos los comentarios
+   public function index ($id)
+   {
+       $post = Post::find($id);
+       if (!$post) {
+           return response()->json([
+               'message' => 'Post no encontrado'
+           ], 403);
+       }
+       return response()->json([
+           'post' => $post->comments()->with('user:id,name,image')->get()
+           ],200);
+   }
+   //crear comentario
+   public function store (Request $request, $id)
+   {
+       $post = Post::find($id);
+       if (!$post) {
+           return response()->json([
+               'message' => 'Post no encontrado'
+           ], 403);
+       }
+       //validacion
+       $request->validate([
+           'comment' => 'required|string',
+       ]);
+       $post = Comment::create([
+           'comment' => $request->comment,
+           'post_id' => $id,
+           'user_id' => auth()->user()->id,
+       ]); 
+       return response()->json([
+           'message' => 'Comentario creado correctamente',
+           'post' => $post->comments()->with('user:id,name,image')->get()
+           ],200);
+
+   }
+
+   //actualizar comentario
+   public function update (Request $request, $id)
+   {
+      $comment = Comment::find($id);
+      if (!$comment) {
+        return response()->json([
+            'message' => 'Comentario no encontrado'
+        ], 403);
+    }
+    if ($comment->user_id != auth()->user()->id) {
+        return response()->json([
+            'message' => 'No tienes permiso para editar este post'
+        ], 403);
+    }
+     //validacion
+     $request->validate([
+        'comment' => 'required|string',
+    ]);
+    $comment->update([
+        'comment' => $request->comment,
+    ]);
+    return response()->json([
+        'message' => 'Comentario actualizado correctamente',
+        'comment' => $comment
+        ],200);
+   }
+   //eliminar comentario
+   public function destroy (Request $request, $id){
+    $comment = Comment::find($id);
+    if (!$comment) {
+        return response()->json([
+            'message' => 'Comentario no encontrado'
+        ], 403);
+    }
+    if ($comment->user_id != auth()->user()->id) {
+        return response()->json([
+            'message' => 'No tienes permiso para eliminar este post'
+        ], 403);
+    }
+    $comment->delete();
+    return response()->json([
+        'message' => 'Comentario eliminado correctamente'
+        ],200);
+   }
 }
--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Like;
 
 class LikeController extends Controller
 {
-    //
+    //like o dislike
+    public function likeOrUnLike ($id){
+      $post = Post::find($id);
+       if (!$post) {
+           return response()->json([
+               'message' => 'Post no encontrado'
+           ], 403);
+        }
+      $like = $post->likes()->where('user_id', auth()->user()->id)->first();
+      //si el usuario no dio like
+      if(!$like){
+        Like::create([
+            'post_id' => $id,
+            'user_id'=> auth()->user()->id
+            ]);
+            return response()->json([
+                'message' => 'Like creado correctamente'
+            ], 200);
+        } 
+      //si el usuario ya dio like
+        $like->delete();
+        return response()->json([
+            'message' => 'Like eliminado correctamente'
+        ], 200);
+       
+    }
+    
 }
